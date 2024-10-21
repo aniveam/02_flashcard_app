1cd03c4542b5d8098214522546153fcdb05bc8a0
aniveam <vinajilliane@gmail.com>
2024-10-20 17:20:00 -0700

    Finish Main Page

--- frontend/src/components/MainHeader.tsx
+++ frontend/src/components/MainHeader.tsx
@@ -1,18 +1,44 @@
 import { useTheme } from "@/context/ThemeContext";
-import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export function MainHeader() {
   const { toggleTheme } = useTheme();
+  const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="w-screen flex flex-row h-16 sticky bg-white dark:bg-theme-dark">
-      <div className="flex-grow" />
-      <div className="flex items-center space-x-3 pr-4">
+    <div className="grid grid-cols-3 w-screen sticky p-4 items-center relative">
+      {/* Link Group */}
+      <div className="col-start-1 col-span-2 md:col-start-2 md:col-span-1 md:place-self-center space-x-10">
+        <Link className="text-black dark:text-white" to="/">
+          Home
+        </Link>
+        <Link className="text-black dark:text-white" to="/">
+          How It Works
+        </Link>
+      </div>
+
+      {/* Hamburger Menu for Small Screens */}
+      <div className="md:hidden flex justify-end">
+        <button
+          onClick={handleMenuToggle}
+          className="text-black dark:text-white">
+          <i className={`fa ${isMenuOpen ? "fa-times" : "fa-bars"} text-2xl`} />
+        </button>
+      </div>
+
+      {/* Buttons for larger screens */}
+      <div className="place-self-end space-x-3 hidden md:flex">
         <motion.button
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
           className="py-2 px-4 bg-slate-900 dark:bg-slate-800 text-white rounded-full">
-          <Link to={"/login"}>Log In</Link>
+          <Link to="/login">Log In</Link>
         </motion.button>
         <motion.button
           whileHover={{ scale: 1.05 }}
@@ -22,6 +48,28 @@ export function MainHeader() {
           <i className="fa-solid fa-circle-half-stroke"></i>
         </motion.button>
       </div>
+
+      {/* Collapsible Menu for Small Screens */}
+      {isMenuOpen && (
+        <div
+          className={`z-50 absolute right-0 top-12 bg-slate-300 dark:bg-gray-600 rounded shadow-lg p-4 md:flex space-x-3 ${isMenuOpen ? "block" : "hidden"} md:hidden`}>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.2 }}
+            className="py-2 px-4 bg-slate-900 dark:bg-slate-800 text-white rounded-full">
+            <Link to={"/login"}>Log In</Link>
+          </motion.button>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.2 }}
+            onClick={toggleTheme}
+            className="py-2 px-4 rounded bg-gray-200 dark:bg-gray-800 text-slate-600 dark:text-white">
+            <i className="fa-solid fa-circle-half-stroke"></i>
+          </motion.button>
+        </div>
+      )}
+
+      {/* Gradient Bottom Line */}
       <div
         className="absolute bottom-0 left-0 w-full h-1"
         style={{
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,34 +1,79 @@
 import { MainHeader } from "@/components/MainHeader";
+import { useTheme } from "@/context/ThemeContext";
 import { motion } from "framer-motion";
 
 export function Main() {
+  const { theme } = useTheme();
+
+  const slideVariants = {
+    start: { y: 40, opacity: 0 },
+    end: { y: 0, opacity: 1 }
+  };
+
+  const bounceVariants = {
+    start: { y: 40 },
+    bounce: { y: [0, 5, 0] }
+  };
+
   return (
-    <div className="flex flex-col items-center h-screen">
+    <div className="h-screen flex flex-col items-center">
       <MainHeader />
-      <motion.div
-        className="mt-[20vh] flex flex-col items-center p-5"
-        initial={{ opacity: 0, y: 40 }} // Initial state: transparent and slightly down
-        animate={{ opacity: 1, y: 0 }} // Final state: fully visible and at normal position
-        transition={{ duration: 0.6 }} // Animation duration
-      >
-        <div>
-          <p className="text-black dark:text-white font-heading text-6xl tracking-wider">
-            Master Concepts, One Flashcard at a Time
-          </p>
-        </div>
-        <div className="w-1/2 p-3 mt-3">
+
+      <div className="mt-[5vh] flex flex-col items-center p-5">
+        <motion.p
+          variants={slideVariants}
+          initial="start"
+          animate="end"
+          transition={{ duration: 0.9 }}
+          className="lg:w-2/3 text-center text-black dark:text-white text-4xl sm:text-5xl lg:text-6xl xl:text-8xl tracking-tighter">
+          Master Concepts, One Flashcard at a Time
+        </motion.p>
+
+        <motion.div
+          variants={slideVariants}
+          initial="start"
+          animate="end"
+          transition={{ duration: 0.9, delay: 0.1 }}
+          className="lg:w-1/2 p-3 mt-3 flex flex-col items-center gap-y-4">
           <p className="text-xl text-gray-600 dark:text-slate-200 text-center">
             Experience effortless learning with a streamlined design—no clutter,
             just the essentials to help you master concepts quickly
           </p>
-        </div>
-        <motion.button
-          className="text-lg rounded-full mt-3 bg-blue-700 text-white py-2 px-4"
-          whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.2 }}>
-          Try it now <i className="fa-regular fa-hand-pointer"></i>
-        </motion.button>
-      </motion.div>
+
+          <motion.button
+            className="text-lg rounded-full bg-blue-600 text-white py-2 px-4"
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.2 }}>
+            Try it now <i className="fa-regular fa-hand-pointer"></i>
+          </motion.button>
+
+          <motion.div
+            variants={bounceVariants}
+            initial="start"
+            animate="bounce"
+            transition={{
+              duration: 2,
+              repeat: Infinity,
+              repeatType: "reverse"
+            }}
+            className="relative p-5 flex justify-center items-center h-auto">
+            <div className="absolute -inset-4 opacity-75 blur">
+              <img
+                className="object-cover w-full h-full h-96"
+                src="/img/Artwork.png"
+                alt="Artwork"
+              />
+            </div>
+            <img
+              className="relative z-10 w-[250px] lg:w-[300px] xl:w-[390px]"
+              src={
+                theme === "light" ? "/img/Macbook.png" : "/img/MacbookDark.png"
+              }
+              alt="MacBook"
+            />
+          </motion.div>
+        </motion.div>
+      </div>
     </div>
   );
 }
